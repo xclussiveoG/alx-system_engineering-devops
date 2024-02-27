35bf22d8cb73a4a393fdff66aa4d47fd6792d015
xclussiveoG <ikireziremymagnom@gmail.com>
2024-02-27 18:41:44 +0000

    regular expressions

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-## This script matches "School" in a string
-#
-#puts ARGV[0].scan(/School/).join
+# This script matches "School" in a string
+
+puts ARGV[0].scan(/School/).join
